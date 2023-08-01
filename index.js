b83dd7dec16f3ade11f4beba04288abfaaa45767
Aidan <aidanchoi@arpainfinity.com>
2023-08-01 10:55:41 +0800

    fix last change

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 const test = false;
+let time = {};
 
 const intervalWaitForMongo = setInterval(async () => {
   try {
