765c43b412b47cff1898028b9f630022899a198d
Aidan <aidanchoi@arpainfinity.com>
2023-08-02 11:47:21 +0800

    fix last change

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const test = false;
 let time = {};
+let interval = null;
 
 const intervalWaitForMongo = setInterval(async () => {
   try {
